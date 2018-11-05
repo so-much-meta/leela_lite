1d745db08eecb0f632cc8ea49c9c66e6d80f2a8d
so-much-meta <trevor.forums@gmail.com>
2018-11-04 23:09:52 -0500

    Optimize UCT algorithm

--- search/uct.py
+++ search/uct.py
@@ -4,6 +4,7 @@ import lcztools
 from lcztools import LeelaBoard
 import chess
 from collections import OrderedDict
+import random
 
 class UCTNode():
     def __init__(self, board=None, parent=None, move=None, prior=0):
@@ -11,7 +12,7 @@ class UCTNode():
         self.move = move
         self.is_expanded = False
         self.parent = parent  # Optional[UCTNode]
-        self.children = OrderedDict()  # Dict[move, UCTNode]
+        self.children = [] # Dict[move, UCTNode]
         self.prior = prior  # float
         self.total_value = 0  # float
         self.number_visits = 0  # int
@@ -22,13 +23,17 @@ class UCTNode():
         else:
             return self.total_value / self.number_visits
 
-    def U(self):  # returns float
-        return (math.sqrt(self.parent.number_visits)
-                * self.prior / (1 + self.number_visits))
+    def U(self, pvsqrt):  # returns float
+        # return random.random()
+        # return (math.sqrt(self.parent.number_visits)
+        #       * self.prior / (1 + self.number_visits))
+        return pvsqrt * self.prior / (1 + self.number_visits)
 
     def best_child(self, C):
-        return max(self.children.values(),
-                   key=lambda node: node.Q() + C*node.U())
+        pvsqrt = self.number_visits ** 0.5
+        def eval_node(node):
+            return node.Q() + C*node.U(pvsqrt)
+        return max(self.children, key=eval_node)
 
     def select_leaf(self, C):
         current = self
@@ -45,7 +50,7 @@ class UCTNode():
             self.add_child(move, prior)
 
     def add_child(self, move, prior):
-        self.children[move] = UCTNode(parent=self, move=move, prior=prior)
+        self.children.append(UCTNode(parent=self, move=move, prior=prior))
     
     def backup(self, value_estimate: float):
         current = self
@@ -84,8 +89,9 @@ def UCT_search(board, num_reads, net=None, C=1.0):
     #for m, node in sorted(root.children.items(),
     #                      key=lambda item: (item[1].number_visits, item[1].Q())):
     #    node.dump(m, C)
-    return max(root.children.items(),
-               key=lambda item: (item[1].number_visits, item[1].Q()))
+    best_node =  max(root.children,
+               key=lambda node: (node.number_visits, node.Q()))
+    return best_node.move, best_node
 
 
 
